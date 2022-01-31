c66ac76267f5610b49fd85966f59f493e81102fd
Mattia Baroni <92336539+bar0x@users.noreply.github.com>
2022-01-31 11:22:40 +0100

    Update prova_funzioni.c

--- funzioni/prova_funzioni.c
+++ funzioni/prova_funzioni.c
@@ -1,3 +1,11 @@
+/* 
+in uno stesso programma realizzare una funzione appropriata per ognuna delle seguenti esigenze :
+stampa una linea con 50 hypen '-'
+stampa una linea con n caratteri c
+stampa un rettangolo di base w, altezza h e carattere c
+il main() del programma chiama le varie funzioni pe rtestarle
+*/
+
 #include <stdio.h>
 
 void PrintLine(){
@@ -10,17 +18,32 @@ void CustomLine(int num, char carattere){
         putchar(carattere);
 }
 
+void PrintRectangle(int w, int h, char c){
+    for (int i=0; i < h; i++){
+        for (int i2=0; i2 < w; i2++)
+            putchar(c);
+            printf ("\n");
+    }
+        
+    
+
+}
+
 
 int main () {
 
     printf("richiamo funzione PrtintLine: \n\n");
     PrintLine();
     printf("\n\n");
-    
+
+
     printf("Richiamo funzione CustomLine: \n\n");
     CustomLine(10, 'f');
-    
- 
+    printf("\n\n");
 
+
+    printf("Richiamo funzione PrintRectangle: \n\n");
+    PrintRectangle(5, 5, 'x');
+    printf("\n\n");
     return 0;
 }
