1189d94479a17602ba4706a915395ae1a1c9b3fc
Mattia Baroni <92336539+bar0x@users.noreply.github.com>
2022-01-31 10:48:03 +0100

    pezione 1
    Introduzione delle funzioni in linguaggio C.

---
+++ funzioni/prova_funzioni.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void PrintLine(){
+    for (int i=0; i < 50; i++)
+        putchar('-');
+}
+
+void CustomLine(int num, char carattere){
+    for (int i=0; i < num; i++)
+        putchar(carattere);
+}
+
+
+int main () {
+
+    printf("richiamo funzione PrtintLine: \n\n");
+    PrintLine();
+    printf("\n\n");
+    
+    printf("Richiamo funzione CustomLine: \n\n");
+    CustomLine(10, 'f');
+    
+
+
+    return 0;
+}
