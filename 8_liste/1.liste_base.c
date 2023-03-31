4d2f519141479fc7e03890889940c45313490896
Mattia Baroni <92336539+bar0x@users.noreply.github.com>
2023-03-31 12:47:34 +0200

    sort reborn

--- 8_liste/1.liste_base.c
+++ 8_liste/1.liste_base.c
@@ -12,6 +12,14 @@ Nodo* addTesta(Nodo *, int);
 void showLista(Nodo *);
 void showListaRecursive(Nodo *);
 void showListaReversedRecursive(Nodo *);
+/**
+ * @brief Ricerca il valore massimo o minimo interno alla lista.
+ * @param Nodo* Puntatore alla testa della lista.
+ * @param int 0->Minimo; 1->Massimo
+ * @return Valore trovato.
+*/
+int ricercaMaxMinLista(Nodo *, int);
+int contaNodi(Nodo*);
 
 // ### MAIN PROOGRAM ###
 int main(){
@@ -50,11 +58,6 @@ void showLista(Nodo *_testa){
         tmp = tmp->next;
     }
 }
-/*
-Address: 11605616; val: 9, next: 11612120
-Address: 11612120; val: 7, next: 11612104
-Address: 11612104; val: 5, next: 0
-*/
 
 void showListaRecursive(Nodo *_tmp){
     if(_tmp != NULL){
@@ -87,3 +90,56 @@ Nodo* pushCoda(Nodo *_Head, int val){
     }
     
 }
+
+int ricercaMaxMinLista(Nodo *_head, int _scelta){
+	// scelta == 0, minimo
+	// scelta == 1, massimo
+	if(_head != NULL){
+		if(_scelta == 1){		// ricerca massimo
+			int max = _head->val;
+			Nodo *_tmp = _head;
+			while ( _tmp != NULL){	// scorro lista
+				if(_tmp->val > max )	// aggiorno massimo
+					max = _tmp->val;
+				_tmp = _tmp->next;
+			}
+			return max;
+		}
+		if(_scelta == 0){		// ricerca minore
+			int min = _head->val;
+			Nodo *_tmp = _head;
+			while ( _tmp != NULL){	// scorro lista
+				if(_tmp->val < min )	// aggiorno minimo
+					min = _tmp->val;
+				_tmp = _tmp->next;
+			}
+			return min;
+		}
+		return -1;	// se parametro errato
+	}
+	return -1;		// se lista vuota
+}
+
+int contaNodi(Nodo* _head){
+	int cnt = 0;	// numero nodi
+	Nodo* _tmp = _head;	// nodo tmp per scorrere
+	while ( _tmp != NULL){			// scorro lista
+		cnt++;
+		_tmp = _tmp->next;
+	}
+	return cnt;		// ritorno numero nodi
+}
+
+Nodo* pushInPos(Nodo* _head, int pos, int val){
+    Nodo* nPos = malloc(sizeof(Nodo));
+    nPos->val = val;
+    nPos->next = 
+    int cnt = contaNodi(Nodo*);
+    Nodo * t;
+    if(nPos == 1){
+        t = addTesta(*_head, val);
+        return t;
+    }
+}
+
+
---
+++ 8_liste/pop_in_position.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definizione dells truttura Nodo
+typedef struct nodo{
+    int val;
+    struct nodo *next;
+}Nodo;
+
+// Dichiarazione dei prototipi
+
+
+// ### MAIN PROOGRAM ###
+int main(){
+    Nodo *t;        // puntatore alla testa della lista.
+   
+    return(0);
+}
+
